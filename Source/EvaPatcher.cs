7d4c5a06adc155b3409cccc1c01fcd47f18d49f6
Nightmare <nightzhuxy@hotmail.com>
2024-05-23 17:08:28 +0800

    chore: Refactor EvaPatcher settings UI

--- Source/EvaPatcher.cs
+++ Source/EvaPatcher.cs
@@ -72,8 +72,11 @@ namespace Template
         {
             base.DoSettingsWindowContents(inRect: inRect);
             Text.Font = GameFont.Medium;
+
+            #region topRect
+            Rect topRect = inRect.TopPart(pct: 0.05f);
             Listing_Standard ls = new Listing_Standard();
-            ls.Begin(inRect);
+            ls.Begin(topRect);
             ls.Label("EvaPatcherGeneralSettings".Translate());
             ls.GapLine(20f);
             ls.Label("Enable EvaPatcher".Translate());
@@ -81,39 +84,55 @@ namespace Template
             ls.GapLine(20f);
             //     ls.CheckboxLabeled("Disable Eva", ref settings.eva, "Disable Eva");
             ls.End();
+            #endregion
+
+            #region leftRect
+            Rect leftRect = inRect.BottomPart(pct: 0.45f).LeftPart(pct: 0.5f);
+            GUI.BeginGroup(position: leftRect, style: new GUIStyle(other: GUI.skin.box));
+
+            GUI.EndGroup();
+            #endregion
+
+            #region rightRect
+            Rect rightRect = inRect.BottomPart(pct: 0.45f).RightPart(pct: 0.5f);
+            GUI.BeginGroup(position: rightRect, style: new GUIStyle(other: GUI.skin.box));
+
+            GUI.EndGroup();
+            #endregion
+
             settings.Write();
         }
     }
 
-
-    [DefOf]
-    public class TemplateDefOf
-    {
-        public static LetterDef success_letter;
-    }
-
-    public class MyMapComponent : MapComponent
-    {
-        public MyMapComponent(Map map) : base(map) { }
-        public override void FinalizeInit()
-        {
-            Messages.Message("Success", null, MessageTypeDefOf.PositiveEvent);
-            Find.LetterStack.ReceiveLetter(new TaggedString("Success"), new TaggedString("Success message"), TemplateDefOf.success_letter, "", 0);
-        }
-    }
-
     [StaticConstructorOnStartup]
-    public static class Start
+    public static class EvaPatcher
     {
-        static Start()
+        static EvaPatcher()
         {
             Log.Message("Mod template loaded successfully!");
 
             // *Uncomment for Harmony*
-            // Harmony harmony = new Harmony("Template");
+            Harmony harmony = new Harmony("rimworld.nightz.sos2EvaPatcher");
+            // harmony.Patch(original: AccessTools.Method(type: typeof(Apparel),name: ApparelUtility.), prefix: null, postfix: new HarmonyMethod(typeof(EvaPatcher).GetMethod("EvaApparelPostfix")));
             // harmony.PatchAll( Assembly.GetExecutingAssembly() );
         }
     }
+    // [DefOf]
+    // public class TemplateDefOf
+    // {
+    //     public static LetterDef success_letter;
+    // }
+
+    // public class MyMapComponent : MapComponent
+    // {
+    //     public MyMapComponent(Map map) : base(map) { }
+    //     public override void FinalizeInit()
+    //     {
+    //         Messages.Message("Success", null, MessageTypeDefOf.PositiveEvent);
+    //         Find.LetterStack.ReceiveLetter(new TaggedString("Success"), new TaggedString("Success message"), TemplateDefOf.success_letter, "", 0);
+    //     }
+    // }
+
 
     // *Uncomment for Harmony*
     // [HarmonyPatch(typeof(LetterStack), "ReceiveLetter")]
