cc88f465d38a40ef1aa5588d6fb6929928a8b1b4
Nightmare <nightzhuxy@hotmail.com>
2024-05-24 11:14:07 +0800

    finish setting left scroll

--- Source/EvaPatcher.cs
+++ Source/EvaPatcher.cs
@@ -29,9 +29,15 @@ namespace EvaPatcher
     {
 
         #region modConfig
+        // first inited in game
         public bool inited = false;
 
+        // enable eva patcher
         public bool enabled = false;
+
+        // patch all apparel with eva tag to eva suit
+        public bool patchEvaTag = false;
+        // all patched eva suit
         public List<ThingDef> eva = new List<ThingDef>();
         #endregion
 
@@ -53,14 +59,29 @@ namespace EvaPatcher
         {
             this.inited = true;
             this.enabled = false;
+            this.patchEvaTag = false; 
             this.eva = new List<ThingDef>();
         }
     }
 
-    internal class Sos2EvaPatcher : Mod
+    internal class Sos2EvaPatchMod : Mod
     {
+        #region variables
         public static Sos2EvaPatchSettings settings;
-        public Sos2EvaPatcher(ModContentPack content) : base(content)
+        public static Sos2EvaPatchMod Instance;
+        #endregion
+
+        #region ui components
+        private string searchTerm = "";
+
+        private ThingDef leftSelectedItem;
+        private ThingDef rightSelectedItem;
+
+        private Vector2 leftScrollPosition;
+        private Vector2 rightScrollPosition;
+        #endregion
+        
+        public Sos2EvaPatchMod(ModContentPack content) : base(content)
         {
             settings = GetSettings<Sos2EvaPatchSettings>();
             // Instance = this;
@@ -73,23 +94,56 @@ namespace EvaPatcher
             base.DoSettingsWindowContents(inRect: inRect);
             Text.Font = GameFont.Medium;
 
+            // get all apparel
+            List<ThingDef> allApparel = DefDatabase<ThingDef>.AllDefs.Where(predicate: x => x.IsApparel).ToList();
+
             #region topRect
             Rect topRect = inRect.TopPart(pct: 0.05f);
             Listing_Standard ls = new Listing_Standard();
             ls.Begin(topRect);
             ls.Label("EvaPatcherGeneralSettings".Translate());
             ls.GapLine(20f);
-            ls.Label("Enable EvaPatcher".Translate());
-            ls.CheckboxLabeled("Enable EvaPatcher".Translate(), ref settings.enabled, "Enable EvaPatcher".Translate());
+            // enable eva patcher
+            ls.Label("EnableEvaPatcher".Translate());
+            ls.CheckboxLabeled("EnableEvaPatcher".Translate(), ref settings.enabled, "EnableEvaPatcher".Translate());
+            // set all appearel has eva tag to eva suit
+            ls.Label("UseEvatag".Translate());
+            ls.CheckboxLabeled("UseEvatag".Translate(), ref settings.enabled, "UseEvatag".Translate());
             ls.GapLine(20f);
-            //     ls.CheckboxLabeled("Disable Eva", ref settings.eva, "Disable Eva");
+            this.searchTerm = Widgets.TextField(rect: topRect.RightPart(pct: 0.95f).LeftPart(pct: 0.95f), text: this.searchTerm);
+            
             ls.End();
             #endregion
 
             #region leftRect
             Rect leftRect = inRect.BottomPart(pct: 0.45f).LeftPart(pct: 0.5f);
             GUI.BeginGroup(position: leftRect, style: new GUIStyle(other: GUI.skin.box));
+            // get left list item
+            List<ThingDef> leftList = allApparel.Where(x=> !settings.eva.Contains(x))
+            .Where(x=> x.apparel.tags.Contains(DefValue.EvaTagName))
+            .Where(x=>  x.label.Contains(this.searchTerm))
+            .ToList();
 
+            float leftItemY = 3f;
+            Widgets.BeginScrollView(outRect: leftRect.AtZero(), scrollPosition: ref this.leftScrollPosition,
+                                    viewRect: new Rect(x: 0f, y: 0f, width: leftRect.width / 10 * 9, height: leftList.Count * 32f));
+            if (!leftList.NullOrEmpty())
+            {
+                foreach (ThingDef def in leftList)
+                {
+                    Rect rowRect = new Rect(x: 5, y: leftItemY, width: leftRect.width - 6, height: 30);
+                    Widgets.DrawHighlightIfMouseover(rect: rowRect);
+                    if (def == this.leftSelectedItem)
+                        Widgets.DrawHighlightSelected(rect: rowRect);
+                    Widgets.Label(rect: rowRect, label: def.LabelCap.RawText ?? def.defName);
+                    if (Widgets.ButtonInvisible(butRect: rowRect))
+                        this.leftSelectedItem = def;
+
+                    leftItemY += 32f;
+                }
+            }
+
+            Widgets.EndScrollView();
             GUI.EndGroup();
             #endregion
 
